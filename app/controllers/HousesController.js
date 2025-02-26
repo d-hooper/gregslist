54ce70b7db1a6dfa08e8c99e07f03d59a8a5ccf4
d-hooper <dallinhoop3@gmail.com>
2025-02-26 14:50:33 -0700

    house controller draws basic house model

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,3 +1,4 @@
+import { AppState } from "../AppState.js";
 
 
 export class HousesController {
@@ -5,12 +6,18 @@ export class HousesController {
   constructor() {
     console.log('Ready to control you domicile! 🏠🎮');
     
+    this.drawHouses()
   }
 
 
 
   drawHouses() {
+    const houses = AppState.houses
+    let houseListingsContent = ''
 
+    houses.forEach(house => houseListingsContent += house.cardHTMLTemplate)
+    const houseListingsElem = document.getElementById('houseListings')
+    houseListingsElem.innerHTML = houseListingsContent
   }
 
 }
